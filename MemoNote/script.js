e73a982d2facbc66fd705ea52a99d5eaff1ab8d8
mariama972 <mbacke.mamediarra@gmail.com>
2024-01-16 15:54:07 +0100

    modif

--- MemoNote/script.js
+++ MemoNote/script.js
@@ -9,6 +9,7 @@ function addNote(){
     else{
         let li = document.createElement("li");
         let input = document.createElement("input");
+        input.type= "text";
         input.classList.add("hide");    /* création d'une classe hide */
         let span = document.createElement("span"); 
         span.classList.add("show")    /* création d'une classe show  */
@@ -17,41 +18,43 @@ function addNote(){
         li.appendChild(span);
         li.appendChild(input);
         listContainer.appendChild(li);
+    
     }
     inputBox.value = "";
     saveData();
 }
-
+let notesArray= localStorage.getItem("data");
+console.log(notesArray);
+    
 function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+    let spans = document.querySelectorAll("#listcontainer span");
+    let contentsArray = [];
+    spans.forEach(function(span) {
+        contentsArray.push(span.innerText);
+    })
+    localStorage.setItem("data", JSON.stringify(contentsArray));
+     
 }
 
 function showNote(){
-    listContainer.innerHTML = localStorage.getItem("data");
+    let noteList = localStorage.getItem("data");
+    console.log(JSON.parse(noteList));
+    listContainer.innerHTML= JSON.parse(noteList);
+
 }
 showNote();
 
-listContainer.addEventListener("click", function(e){
-    if (e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-    }
-    else if (e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-    }
-}, false);
-
-
-
-
-
-
 /* fonction qui permet de modifier la note, 
 on peut en cliquant directement sur la note la modifier */
-const listLi = document.querySelectorAll("#listcontainer li");
+const listLi = document.querySelectorAll("#listcontainer li span ");
 for (i=0; i<listLi.length; i++){
     listLi[i].addEventListener("click", change)
 }
 function change(event){
+    event.target.classList.remove("show");
+    event.target.classList.add("hide");
+    event.target.nextSibling.classList.remove("hide");
+    event.target.nextSibling.classList.add("show");
+    
 
-    alert ("ecriver")
 }
